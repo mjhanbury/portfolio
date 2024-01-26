ec3d4b07aea2d4684704360524e2466c3997e4e8
Miles Hanbury <jpage@retailbackbone.com>
2024-01-25 19:05:33 -0500

    added header buttons and base orbit

---
+++ src/Animation.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import './Animation.css'
+
+const Object = ({className, offset}) => {
+
+	const [angle, setAngle] = useState(offset);
+	const [radius, setRadius] = useState(1);
+
+	useEffect(() => {
+		const onScroll = e => {
+			setRadius((window.innerHeight - e.target.documentElement.scrollTop)/window.innerHeight);
+		};
+		window.addEventListener("scroll", onScroll);
+
+		return () => window.removeEventListener("srcoll", onScroll);
+	}, [radius]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setAngle((prevAngle) => prevAngle + 0.01);
+		}, 10);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<div 
+			className={className}
+			style={{
+				marginBottom: `${25 * radius * Math.cos(angle)}rem`,
+				marginLeft: `${25 * radius * Math.sin(angle)}rem`
+			}}
+		>
+
+		</div>
+	);
+}
+
+export default function Animation() {
+	return (
+		<>
+			<div className="Animation-path"/>
+			<Object className="particle" offset={0}/>
+			<Object className="particle" offset={Math.PI }/>
+		</>
+	);
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload, faContactCard } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Animation from './Animation.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className='App-header'>
+				<div className='App-downloads'>
+					<button>
+						<p>Resume</p>
+						<FontAwesomeIcon icon={faDownload} className="icon"></FontAwesomeIcon>
+					</button>
+					<button>
+						<p>CV</p>
+						<FontAwesomeIcon icon={faDownload} className="icon"></FontAwesomeIcon>
+					</button>
+				</div>
+				<div className='App-media'>
+					<FontAwesomeIcon icon={faGithub} className="icon"></FontAwesomeIcon>
+					<FontAwesomeIcon icon={faLinkedin} className="icon"></FontAwesomeIcon>
+					<FontAwesomeIcon icon={faContactCard} className="icon"></FontAwesomeIcon>
+				</div>
+			</div>
+
+			<div className='App-body'>
+				<Animation/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
